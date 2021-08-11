31934821a98397cf72e6a3a3eef9ec0774384e9e
apal <alishapal96@gmail.com>
2021-08-10 17:40:26 -0700

    add styling and image

--- public/js/work.js
+++ public/js/work.js
@@ -1,9 +1,8 @@
+function viewHomepage(){
+    document.location.replace("/");
+}
 function viewWork(){
     document.location.replace("/work");
 }
-
-
-
-
-
+document.querySelector("#about-me").addEventListener("click", viewHomepage);
 document.querySelector("#work").addEventListener("click", viewWork);
